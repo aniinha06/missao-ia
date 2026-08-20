0d46efdf897d719dfaff899322804bc86532d273
aniinha06 <toigo.ana@escola.pr.gov.br>
2026-08-19 21:21:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,9 @@ const textoResultado = document.querySelector(".texto-resultado");
 ]
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
+
+
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
@@ -60,13 +63,18 @@ function mostraPergunta() {
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativa = document.createElement("button");
-        botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function(){
-            atual++,
-            mostraPergunta();
-        })
-        caixaAlternativas.appendChild(botaoAlternativa);
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+mostraPergunta();
 }
 mostraPergunta()
