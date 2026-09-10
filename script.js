f2ff194774021eea9754560ee1a87a99c51e74d8
aniinha06 <toigo.ana@escola.pr.gov.br>
2026-09-09 22:28:06 -0300

    Enhance script.js with name substitution functionality
    Updated the import statement to include 'nome' and modified the 'mostraResultado' function to use 'nome' in the text content. Added 'substituiNome' function to replace 'você' with 'nome' in questions.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {aleatorio} from './aleatorio.js';
+import {aleatorio, nome} from './aleatorio.js';
 import {perguntas} from './perguntas.js';
 
 const caixaPrincipal = document.querySelector(".caixa-principal");
@@ -91,7 +91,7 @@ function respostaSelecionada(opcaoSelecionada){
 
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, ${nome}`;
     textoResultado.textContent historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
@@ -105,4 +105,11 @@ function jogaNovamente(){
         mostraPergunta();
 }
 
+function substituiNome(){
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+
+substituiNome();
 mostraPergunta();
