f881d6aa1452593115f0c8adb294a9b1e565d1ef
aniinha06 <toigo.ana@escola.pr.gov.br>
2026-09-09 22:34:49 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -14,13 +14,15 @@ export const perguntas = [
             "No início ficou com medo do que essa tecnologia pode fazer.",
             "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
         ]
+        proxima: 1,
     },
     {
         texto: "Isso é maravilhoso!",
         afirmacao: [
             "Quis saber como usar IA no seu dia a dia.",
             "Pensou que IA pode ajudar em tarefas da sua vida."
-        ]
+        ],
+            proxima: 2,
     }
 ]
 
@@ -30,6 +32,7 @@ export const perguntas = [
             "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
         ]
+        proxima: 3,
     },
     {
         enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?
@@ -38,6 +41,7 @@ export const perguntas = [
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
         ]
+        proxima:4,
     },
     {
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
@@ -45,6 +49,7 @@ export const perguntas = [
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
             "Criar uma imagem utilizando um gerador de imagem de IA."
         ]
+        proxima: 5,
     },
     {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
@@ -52,6 +57,7 @@ export const perguntas = [
             "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
         ]
+        proxima: 6,
     }
 ]
 let atual = 0;
